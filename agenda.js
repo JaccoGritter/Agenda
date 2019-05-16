a733cadb4e53f4d5efbe3c52ada61b1f755181b2
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-16 20:34:41 +0200

    code optimization

--- agenda.js
+++ agenda.js
@@ -21,6 +21,7 @@ class agendaEntry {
 
 function updateCards(number) {
 
+    number -= 2;       // get current day in the middle card
     let cardheader = document.getElementsByClassName("card-header");
     let cardbody = document.getElementsByClassName("card-body");
     for (let i=0; i<3; i++) {
@@ -35,8 +36,7 @@ function calculateDate(daynumber) {
     if (daynumber < 1 || daynumber >365) return;
     let month = 0;
     let day = 0;
-    for (let i = 0; i<13; i++) {
-        
+    for (let i = 0; i<13; i++) {     
         if (daynumber <= monthsBreakpoints[i]) {
             month = i;
             day = daynumber - monthsBreakpoints[i-1];
@@ -56,27 +56,26 @@ function calculateDayNo(month, day) {
 function goLeft1() {
     daynoToday -= 1;
     if(daynoToday < 1) daynoToday = 2;
-    updateCards(daynoToday-2);
+    updateCards(daynoToday);
 }
 
 function goLeft7() {
-    daynoToday -= 5;                // element will catch the single click as well!
+    daynoToday -= 5;                // element will handle the single clicks as well!
     if(daynoToday < 1) daynoToday = 2;
-    updateCards(daynoToday-2);
+    updateCards(daynoToday);
 }
 
 function goRight1() {
     daynoToday += 1;
-    updateCards(daynoToday-2);
+    updateCards(daynoToday);
 }
 
 function goRight7() {
-    daynoToday += 5;                // element will catch the single click as well!
-    updateCards(daynoToday-2);
+    daynoToday += 5;                // element will handle the single clicks as well!
+    updateCards(daynoToday);
 }
 
 function edit() {
-    //console.log("edit screen");
     document.getElementById("form").style.display = "block";
     document.getElementById("form").value = agenda2019[daynoToday-1].getTodo();
     if (document.getElementById("form").value == " * empty * ") document.getElementById("form").value = "";
@@ -86,7 +85,7 @@ function processInput() {
     agenda2019[daynoToday-1].setTodo( document.getElementById("form").value );
     document.getElementById("form").value = "";
     document.getElementById("form").style.display = "none";
-    updateCards(daynoToday-2);
+    updateCards(daynoToday);
 }
 
 document.getElementById("arrowLeft").addEventListener("dblclick", goLeft7);
@@ -99,25 +98,20 @@ document.getElementById("arrowDown").addEventListener("click", goRight1);
 document.getElementById("edit").addEventListener("click", edit);
 
 
-
+// Action starts here
 let agenda2019 = [];
 for (let i = 0; i < 365; i++) {
     agenda2019.push(new agendaEntry(i+1, " * empty * "));
 }
 
-// for testing purposes
-// agenda2019[133] = new agendaEntry(134, "Bootstrap theorie. Pagina bouwen met behulp van bootstrap");
-// agenda2019[134] = new agendaEntry(135, "SASS theorie doornemen. Pagina bouwen met behulp van SASS");
-// agenda2019[135] = new agendaEntry(136, "JavaScript Design patterns. Verder werken aan bestaande projecten");
-
 let d = new Date();
 let daynoToday = calculateDayNo(d.getMonth(),d.getDate());
 
 // for testing purposes
-    agenda2019[daynoToday-2] = new agendaEntry((daynoToday-1), "Bootstrap theorie. Pagina bouwen met behulp van bootstrap");
-    agenda2019[daynoToday-1] = new agendaEntry((daynoToday), "SASS theorie doornemen. Pagina bouwen met behulp van SASS");
-    agenda2019[daynoToday] = new agendaEntry((daynoToday+1), "JavaScript Design patterns. Verder werken aan bestaande projecten");
+agenda2019[daynoToday-2] = new agendaEntry((daynoToday-1), "Bootstrap theorie. Pagina bouwen met behulp van bootstrap");
+agenda2019[daynoToday-1] = new agendaEntry((daynoToday), "SASS theorie doornemen. Pagina bouwen met behulp van SASS");
+agenda2019[daynoToday] = new agendaEntry((daynoToday+1), "JavaScript Design patterns. Verder werken aan bestaande projecten");
 
 
-updateCards(daynoToday-2);   // get current day in the middle
+updateCards(daynoToday);   
 
